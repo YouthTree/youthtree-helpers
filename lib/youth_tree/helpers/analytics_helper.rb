e7dd8f6fe8e16afc08d177aa8d8d6fe228e7f44b
Darcy Laycock <sutto@sutto.net>
2010-08-23 02:30:31 +0800

    Add updated js to fix it

--- lib/youth_tree/helpers/analytics_helper.rb
+++ lib/youth_tree/helpers/analytics_helper.rb
@@ -10,7 +10,7 @@ module YouthTree
             var s = document.createElement('script');
             s.type = 'text/javascript';
             s.async = true;
-            s.src = (document.location.protocol + '://static.getclicky.com/js');
+            s.src = (document.location.protocol + '//static.getclicky.com/js');
             (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(s);
           })();
         END_OF_JS
@@ -21,7 +21,7 @@ module YouthTree
           content = ActiveSupport::SafeBuffer.new
           site_id = Settings.clicky.site_id.to_i;
           content << javascript_tag(clicky_snippet_js(site_id))
-          static_image_tag = tag(:img, :width => 1, :height => 1, :src => "#{request.protocol}://in.getclicky.com/#{site_id}ns.gif")
+          static_image_tag = tag(:img, :width => 1, :height => 1, :src => "#{request.protocol}in.getclicky.com/#{site_id}ns.gif")
           content << content_tag(:noscript, content_tag(:div, static_image_tag, :class => 'clicky-tracker'))
           content
         end
